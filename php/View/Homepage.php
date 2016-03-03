4f57d752120bc4a6ba7976c0e36be233aac5fe40
ferryzijl <fjgzijlmans@hotmail.com>
2016-03-03 09:01:20 +0100

    - started php project
    -- added the pages
    -- added php assets

---
+++ php/View/Homepage.php
@@ -0,0 +1,300 @@
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8"> <![endif]-->  
+<!--[if IE 9]> <html lang="en" class="ie9"> <![endif]-->  
+<!--[if !IE]><!--> <html lang="en"> <!--<![endif]-->  
+<head>
+    <title>Alladin | Home</title>
+	<?php include 'Assets/Head.php';?>
+
+</head>	
+
+<body>    
+
+<div class="wrapper">    
+   <?php include 'Assets/NavTop.php';?>
+
+    <!--=== Slider ===-->
+    <div id="layerslider" style="width: 100%; height: 500px; margin: 0px auto;">
+        <!-- First slide -->
+        <div class="ls-slide" data-ls="slidedelay:4500;transition2d:25;">
+            <img src="assets/img/sliders/layer/bg1.jpg" class="ls-bg" alt="Slide background"/>
+
+            <img src="images/jasmine.png" alt="Slider image" class="ls-s-1" style=" top:62px; left: 29%;" 
+            data-ls="offsetxin:left; durationin:1500; delayin:1500; fadein:false; offsetxout:left; durationout:1000; fadeout:false;">
+        
+            <span class="ls-s-1" style=" text-transform: uppercase; line-height: 45px; font-size:35px; color:#fff; top:200px; left: 590px; slidedirection : top; slideoutdirection : bottom; durationin : 3500; durationout : 3500; delayin : 1000;">
+                Voorbeel text
+            </span>
+        </div>
+
+        <!--Second Slide-->
+        <div class="ls-slide" data-ls="transition2d:93;">
+            <img src="assets/img/bg/18.jpg" class="ls-bg" alt="Slide background">
+
+            <i class="fa fa-chevron-circle-right ls-s-1" style=" color: #fff; font-size: 24px; top:70px; left: 40px; slidedirection : left; slideoutdirection : top; durationin : 1500; durationout : 500; "></i> 
+
+            <span class="ls-s-2" style=" color: #fff; font-weight: 200; font-size: 22px; top:70px; left: 70px; slidedirection : top; slideoutdirection : bottom; durationin : 1500; durationout : 500; ">
+                reponsive en aanpasbaar
+            </span>
+
+            <i class="fa fa-chevron-circle-right ls-s-1" style=" color: #fff; font-size: 24px; top:120px; left: 40px; slidedirection : left; slideoutdirection : top; durationin : 2500; durationout : 1500; "></i> 
+
+            <span class="ls-s-2" style=" color: #fff; font-weight: 200; font-size: 22px; top:120px; left: 70px; slidedirection : top; slideoutdirection : bottom; durationin : 2500; durationout : 1500; ">
+                verschillende sliders voor een dynamische weergaven
+            </span>
+
+            <i class="fa fa-chevron-circle-right ls-s-1" style=" color: #fff; font-size: 24px; top:170px; left: 40px; slidedirection : left; slideoutdirection : top; durationin : 3500; durationout : 3500; "></i> 
+
+            <span class="ls-s-2" style=" color: #fff; font-weight: 200; font-size: 22px; top:170px; left: 70px; slidedirection : top; slideoutdirection : bottom; durationin : 3500; durationout : 2500; ">
+                gebruik van icons
+            </span>
+
+            <a class="btn-u btn-u-blue ls-s1" href="#" style=" padding: 9px 20px; font-size:25px; top:340px; left: 40px; slidedirection : bottom; slideoutdirection : bottom; durationin : 6500; durationout : 3500; ">
+                gebaseerd op Bootstrap 3
+            </a>
+
+            <img src="images/jasmine.png" alt="Slider Image" class="ls-s-1" style=" top:30px; left: 650px; slidedirection : right; slideoutdirection : bottom; durationin : 1500; durationout : 1500; ">
+        </div>                
+
+        <!--Third Slide-->
+        <div class="ls-slide" style="slidedirection: right; transition2d: 92,93,105; ">
+            <img src="assets/img/sliders/layer/bg2.jpg" class="ls-bg" alt="Slide background">
+        </div>
+        <!--End Third Slide-->
+    </div><!--/layer_slider-->
+    <!--=== End Slider ===-->
+
+    <!--=== Content Part ===-->
+    <div class="container content">
+        <div class="margin-bottom-10"></div>
+        <!-- Service Blocks -->
+        <div class="row">
+        	<div class="col-md-12">
+        	<h2 class="heading-sm">
+                 <a class="link-bg-icon" href="#">
+                     <i class="icon-custom icon-sm rounded-x icon-bg-dark fa fa-thumbs-o-up"></i> 
+                     <span>Alladin</span>
+                 </a>
+            </h2>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nunc dolor, volutpat id massa vitae, rhoncus dictum dolor. Suspendisse quis vehicula lectus. Nam ornare nunc id dignissim placerat. Suspendisse potenti. In consequat luctus lectus, suscipit porta est. Pellentesque iaculis efficitur ex, vitae pulvinar dolor commodo id. Integer eget elit non elit lobortis posuere vitae eu mi. Fusce pellentesque leo nunc, vitae condimentum dolor lacinia non. Aliquam congue vitae purus suscipit sodales. Integer ac blandit nulla, eu convallis tellus.
+
+Maecenas orci justo, suscipit eu nisi eu, rutrum eleifend nibh. Pellentesque in tortor eu elit cursus ultricies. Nullam posuere consequat lacus eget gravida. Vestibulum nec congue diam. Nunc efficitur ultrices enim, a hendrerit lectus accumsan eu. Donec non metus turpis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam quis gravida risus, nec vehicula turpis.
+            </p>
+        	
+        	</div>
+        	<div class="col-md-12">
+        	        	<h2 class="heading-sm">
+                 <a class="link-bg-icon" href="#">
+                     <i class="icon-custom icon-sm rounded-x icon-bg-dark fa fa-info"></i> 
+                     <span>Hoe werkt het?</span>
+                 </a>
+            </h2>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nunc dolor, volutpat id massa vitae, rhoncus dictum dolor. Suspendisse quis vehicula lectus. Nam ornare nunc id dignissim placerat. Suspendisse potenti. In consequat luctus lectus, suscipit porta est. Pellentesque iaculis efficitur ex, vitae pulvinar dolor commodo id. Integer eget elit non elit lobortis posuere vitae eu mi. Fusce pellentesque leo nunc, vitae condimentum dolor lacinia non. Aliquam congue vitae purus suscipit sodales. Integer ac blandit nulla, eu convallis tellus.
+
+Maecenas orci justo, suscipit eu nisi eu, rutrum eleifend nibh. Pellentesque in tortor eu elit cursus ultricies. Nullam posuere consequat lacus eget gravida. Vestibulum nec congue diam. Nunc efficitur ultrices enim, a hendrerit lectus accumsan eu. Donec non metus turpis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam quis gravida risus, nec vehicula turpis.
+            </p>
+        	</div>
+        
+        </div>
+        <!-- End Service Blokcs --> 
+        
+        <!-- Info Blokcs -->
+    	<div class="row margin-bottom-20">
+        	<!-- Welcome Block -->
+    		<div class="col-md-12 md-margin-bottom-40">
+    			<div class="headline"><h2>Hallo, dit is Alladin</h2></div>
+                <div class="row">
+                    <div class="col-sm-4">
+                        <img class="img-responsive margin-bottom-20" src="assets/img/main/img12.jpg" alt="">
+                    </div>
+                    <div class="col-sm-8">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nunc dolor</p>
+                        <ul class="list-unstyled margin-bottom-20">
+                            <li><i class="fa fa-check color-green"></i> consectetur adipiscing elit.</li>
+                            <li><i class="fa fa-check color-green"></i> consectetur adipiscing elit.</li>
+                            <li><i class="fa fa-check color-green"></i> consectetur adipiscing elit.</li>
+                            <li><i class="fa fa-check color-green"></i> consectetur adipiscing elit.</li>
+                        </ul>   
+                        <button type="button" class="btn btn-danger ">Meld je nu aan!</button>                 
+                    </div>
+                    
+                </div>
+				<br />
+                <blockquote class="hero-unify">
+                    <p>Alladin is een leuk initiatief en heeft veel potentie.</p>
+                    <small>Karel</small>
+                </blockquote>
+            </div><!--/col-md-8-->        
+    	</div><!--/row-->	
+    	<!-- End Info Blokcs -->   
+
+        <!-- Nieuws -->
+        <div class="owl-carousel-v1 owl-work-v1 margin-bottom-40">
+            <div class="headline"><h2 class="pull-left">Nieuws</h2>
+                <div class="owl-navigation">
+                    <div class="customNavigation">
+                        <a class="owl-btn prev-v2"><i class="fa fa-angle-left"></i></a>
+                        <a class="owl-btn next-v2"><i class="fa fa-angle-right"></i></a>
+                    </div>
+                </div><!--/navigation-->
+            </div>
+
+            <div class="owl-recent-works-v1">
+                <div class="item">
+                    <a href="#">
+                        <em class="overflow-hidden">
+                            <img class="img-responsive" src="assets/img/main/img1.jpg" alt="">
+                        </em>    
+                        <span>
+                            <strong>Gelukkig nieuwjaar!</strong>
+                            <i>Anim pariatur cliche reprehenderit</i>
+                        </span>
+                    </a>    
+                </div>
+                <div class="item">
+                    <a href="#">
+                        <em class="overflow-hidden">
+                            <img class="img-responsive" src="assets/img/main/img2.jpg" alt="">
+                        </em>    
+                        <span>
+                            <strong>Nieuwe website!</strong>
+                            <i>Responsive Bootstrap website</i>
+                        </span>
+                    </a>    
+                </div>
+                <div class="item">
+                    <a href="#">
+                        <em class="overflow-hidden">
+                            <img class="img-responsive" src="assets/img/main/img3.jpg" alt="">
+                        </em>    
+                        <span>
+                            <strong>Lorem ipsum</strong>
+                            <i>Pariatur prehe cliche reprehrit</i>
+                        </span>
+                    </a>    
+                </div>
+                <div class="item">
+                    <a href="#">
+                        <em class="overflow-hidden">
+                            <img class="img-responsive" src="assets/img/main/img4.jpg" alt="">
+                        </em>    
+                        <span>
+                            <strong>Lorem ipsum</strong>
+                            <i>Craft labore wes anderson cred</i>
+                        </span>
+                    </a>    
+                </div>
+                <div class="item">
+                    <a href="#">
+                        <em class="overflow-hidden">
+                            <img class="img-responsive" src="assets/img/main/img5.jpg" alt="">
+                        </em>    
+                        <span>
+                            <strong>Lorem ipsum</strong>
+                            <i>Anim pariatur cliche reprehenderit</i>
+                        </span>
+                    </a>    
+                </div>
+                <div class="item">
+                    <a href="#">
+                        <em class="overflow-hidden">
+                            <img class="img-responsive" src="assets/img/main/img1.jpg" alt="">
+                        </em>    
+                        <span>
+                            <strong>Lorem ipsum</strong>
+                            <i>Anim pariatur cliche reprehenderit</i>
+                        </span>
+                    </a>    
+                </div>
+                <div class="item">
+                    <a href="#">
+                        <em class="overflow-hidden">
+                            <img class="img-responsive" src="assets/img/main/img2.jpg" alt="">
+                        </em>    
+                        <span>
+                            <strong>Lorem ipsum</strong>
+                            <i>Responsive Bootstrap Template</i>
+                        </span>
+                    </a>    
+                </div>
+                <div class="item">
+                    <a href="#">
+                        <em class="overflow-hidden">
+                            <img class="img-responsive" src="assets/img/main/img3.jpg" alt="">
+                        </em>    
+                        <span>
+                            <strong>Lorem ipsum</strong>
+                            <i>Pariatur prehe cliche reprehrit</i>
+                        </span>
+                    </a>    
+                </div>
+                <div class="item">
+                    <a href="#">
+                        <em class="overflow-hidden">
+                            <img class="img-responsive" src="assets/img/main/img4.jpg" alt="">
+                        </em>    
+                        <span>
+                            <strong>Lorem ipsum</strong>
+                            <i>Craft labore wes anderson cred</i>
+                        </span>
+                    </a>    
+                </div>
+                <div class="item">
+                    <a href="#">
+                        <em class="overflow-hidden">
+                            <img class="img-responsive" src="assets/img/main/img5.jpg" alt="">
+                        </em>    
+                        <span>
+                            <strong>Lorem ipsum</strong>
+                            <i>Anim pariatur cliche reprehenderit</i>
+                        </span>
+                    </a>    
+                </div>
+            </div>
+        </div>
+        <!-- End Recent Works -->
+
+        <!-- Owl Clients v1 -->
+        <div class="headline"><h2>Medemogelijk gemaakt door:</h2></div>        
+        <div class="owl-clients-v1">
+            <div class="item">
+                <img src="assets/img/clients4/1.png" alt="">
+            </div>
+            <div class="item">
+                <img src="assets/img/clients4/2.png" alt="">
+            </div>
+            <div class="item">
+                <img src="assets/img/clients4/3.png" alt="">
+            </div>
+            <div class="item">
+                <img src="assets/img/clients4/4.png" alt="">
+            </div>
+            <div class="item">
+                <img src="assets/img/clients4/5.png" alt="">
+            </div>
+            <div class="item">
+                <img src="assets/img/clients4/6.png" alt="">
+            </div>
+            <div class="item">
+                <img src="assets/img/clients4/7.png" alt="">
+            </div>
+            <div class="item">
+                <img src="assets/img/clients4/8.png" alt="">
+            </div>
+            <div class="item">
+                <img src="assets/img/clients4/9.png" alt="">
+            </div>
+        </div>
+        <!-- End Owl Clients v1 -->        
+    </div><!--/container-->		
+    <!-- End Content Part -->
+	<?php include 'Assets/Footer.php';?>
+   
+</div><!--/wrapper-->
+
+<?php include 'Assets/jsCall.php';?>
+
+</body>
+</html>	
