9c1a4bad8e1455de5f1283a799943a451cd3f083
Mick <mdepeinder@gmail.com>
2016-03-10 21:11:34 +0100

    ddfd

--- php/DB/Userinsert.class.php
+++ php/DB/Userinsert.class.php
@@ -16,7 +16,7 @@ function insert_user($sql, $voornaam, $achternaam, $tussenvoegsel, $date, $email
 	echo $wachtwoord."<br>";
 		
 	// 		$query = "INSERT INTO testing (id, naam, achternaam, nummer) VALUES ('1', 'mick', 'dep', '0634903897')";
-	$query = "INSERT INTO account (gebruikersnaam, voornaam, achternaam, tussenvoegsel, geboortedatum, email, straatnaam, huisnummer, postcode, woonplaats, geslacht, rechten, wachtwoord) VALUES ('Mickey10', '$voornaam', '$achternaam', '$tussenvoegsel' , '$date', '$email' , '$straatnaam', '$huisnummer', '$postcode', '$woonplaats', '$geslacht', '$rechten', '$wachtwoord')";
+	$query = "INSERT INTO account (gebruikersnaam, voornaam, achternaam, tussenvoegsel, geboortedatum, email, straatnaam, huisnummer, postcode, woonplaats, geslacht, rechten, wachtwoord) VALUES ('Mickey14', '$voornaam', '$achternaam', '$tussenvoegsel' , '$date', '$email' , '$straatnaam', '$huisnummer', '$postcode', '$woonplaats', '$geslacht', '$rechten', '$wachtwoord')";
 	if (mysqli_query($sql, $query)) {
 		header('location: ../View/SuccesPage.php');
 	} else {
