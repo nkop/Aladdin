268b66e88d2c1fa2f5b49a7cc058d99dd575562c
WouterAvans <wven2@avans.nl>
2016-03-14 13:01:20 +0100

    Personal Info page ready

--- php/View/page_personalinfo.php
+++ php/View/page_personalinfo.php
@@ -6,7 +6,6 @@
 <!--<![endif]-->
 <head>
 <title>Alladin | Home</title>
-
 <?php include 'Assets/php/Head.php';?>
 </head>
 
@@ -16,6 +15,7 @@
 <?php include 'assets/php/NavTop.php';?>
 <?php include '../DB/Database.class.php';?>
 <?php include '../Controller/PersonalInfo.php'?>
+<?php include '../Controller/UserLogin.php'?>
     <!--=== Breadcrumbs ===-->
 		<div class="breadcrumbs">
 			<div class="container">
@@ -36,18 +36,18 @@
 		<div class="container content">
 			<div class="row">
 				<div class="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
-					<form action="Homepage.php" method="POST" class="reg-page" >
+					<form name="selectionform" action="page_personalinfo.php" method="POST" class="reg-page" onchange="selectionform.submit()" >
 						<div class="reg-header">
 							<h2>Persoonlijke informatie</h2>
 						</div>
 						
 						<?php
+					$username = $_SESSION['email'];
 					$db = Database::getInstance ();
 					$mysqli = $db->getConnection ();
-					$sql_query = "select * from account where accountid = '20'";
+					$sql_query = "select * from account where email = '$username' ";
 					$result = $mysqli->query ( $sql_query );
-					
-					while ( $row = $result->fetch_object () ) {
+					while ( $row = $result->fetch_object() ) {
 						$ID = $row->accountid;
 						$Firstname = $row->voornaam;
 						$Middlename = $row->tussenvoegsel;
@@ -56,47 +56,51 @@
 						$ZipCode = $row->postcode;
 						$City = $row->woonplaats;
 						$Street = $row->straatnaam;
+						$Birthday = $row->geboortedatum;
 						$HouseNumber = $row->huisnummer;
 					}
 					$result->close ();
 					?>
 					              											
                    		<label>Voornaam</label>
-                   		<input type="text" name="FirstNameInput" value=<?php echo $Firstname?> class="form-control margin-bottom-20"> 
+                   		<input type="text" name="FirstNameInput" value="<?php echo $Firstname?>" class="form-control margin-bottom-20"> 
 							
 						<div class="row">
 							<div class="col-sm-6">
 								<label>Tussenvoegsel</label>
-								<input type="text" name="MiddleNameInput" value=<?php echo $Middlename?> class="form-control margin-bottom-20"> 
+								<input type="text" name="MiddleNameInput" value="<?php echo $Middlename?>" class="form-control margin-bottom-20"> 
 							</div>
 							<div class="col-sm-6">	
 								<label>Achternaam</label>
-								<input type="text" name="LastNameInput" value=<?php echo $Lastname?> class="form-control margin-bottom-20"> 
+								<input type="text" name="LastNameInput" value="<?php echo $Lastname?>" class="form-control margin-bottom-20"> 
 							</div>
 						</div>
 						
 						<label>Emailadres</label>
-						<input type="text" name="EmailInput" value=<?php echo $Emailaddress?> class="form-control margin-bottom-20">
+						<input type="text" name="EmailInput" value="<?php echo $Emailaddress?>" class="form-control margin-bottom-20">
 
+						<label>Geboortedatum</label>
+						<input type="text" name="BirthdayInput" value="<?php echo $Birthday?>" class="form-control margin-bottom-20" disabled>
+						
 						<div class="row">
 							<div class="col-sm-6">
 								<label>Straat</label> 
-								<input type="text" name="StreetInput" value=<?php echo $Street?> class="form-control margin-bottom-20">
+								<input type="text" name="StreetInput" value="<?php echo $Street?>" class="form-control margin-bottom-20">
 							</div>
 							<div class="col-sm-6">
 								<label>Huisnummer</label> 
-								<input type="text" name="HouseNumberInput" value=<?php echo $HouseNumber?> class="form-control margin-bottom-20">
+								<input type="text" name="HouseNumberInput" value="<?php echo $HouseNumber?>" class="form-control margin-bottom-20">
 							</div>
 						</div>
 						
 						<div class="row">
 							<div class="col-sm-6">
 								<label>Postcode</label> 
-								<input type="text" name="ZipCodeInput" value=<?php echo $ZipCode?> class="form-control margin-bottom-20">
+								<input type="text" name="ZipCodeInput" value="<?php echo $ZipCode?>" class="form-control margin-bottom-20">
 							</div>
 							<div class="col-sm-6">
-								<label>City</label> 
-								<input type="text" name="CityInput" value=<?php echo $City?> class="form-control margin-bottom-20">
+								<label>Woonplaats</label> 
+								<input type="text" name="CityInput" value="<?php echo $City?>" class="form-control margin-bottom-20">
 							</div>
 						</div>
 
@@ -112,8 +116,10 @@
 					</form>
                <?php
 				if (isset ( $_POST ['submit'] ) && isset ( $_POST ['accountid'] ) 
-						&& isset ( $_POST ['FirstNameInput'] ) && isset ( $_POST ['MiddleNameInput'] ) && isset ( $_POST ['LastNameInput'] ) && isset ( $_POST ['ZipCodeInput'] ) && isset ( $_POST ['HouseNumberInput'] ) && isset ( $_POST ['StreetInput'] ) && isset ( $_POST ['CityInput'] )&& isset ( $_POST ['EmailInput'] )) {
-				validateData( $_POST ['accountid'], $_POST ['FirstNameInput'], $_POST ['MiddleNameInput'], $_POST ['LastNameInput'], $_POST ['ZipCodeInput'], $_POST ['HouseNumberInput'], $_POST ['StreetInput'], $_POST ['CityInput'], $_POST['EmailInput'] );
+						&& isset ( $_POST ['FirstNameInput'] ) && isset ( $_POST ['LastNameInput'] ) && isset ( $_POST ['ZipCodeInput'] ) && isset ( $_POST ['HouseNumberInput'] ) && isset ( $_POST ['StreetInput'] ) && isset ( $_POST ['CityInput'] )&& isset ( $_POST ['EmailInput'] )) {
+							validateData( $_POST ['accountid'], $_POST ['FirstNameInput'], $_POST ['MiddleNameInput'], $_POST ['LastNameInput'], $_POST ['ZipCodeInput'], $_POST ['HouseNumberInput'], $_POST ['StreetInput'], $_POST ['CityInput'], $_POST['EmailInput'] );
+							echo '<META HTTP-EQUIV="Refresh" Content="0; URL=page_personalinfo.php">';
+							exit;
 						}
 			   ?> 
             </div>
