d4dc290c94a37e838a87c3b961a5bddf91bb709f
Mick <mdepeinder@gmail.com>
2016-03-16 00:55:59 +0100

    wwe

--- php/View/Assets/js/passwordCheck.js
+++ php/View/Assets/js/passwordCheck.js
@@ -1,6 +1,6 @@
     function check(input) {
         if (input.value != document.getElementById('password').value) {
-            input.setCustomValidity('Password Must be Matching.');
+            input.setCustomValidity('Wachtwoorden moeten gelijk zijn');
         } else {
             // input is valid -- reset the error message
             input.setCustomValidity('');
