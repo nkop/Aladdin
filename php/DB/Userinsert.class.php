5f57e7d399e1cf1a38a1a7134288e7182b139154
Mick <mdepeinder@gmail.com>
2016-03-22 23:02:28 +0100

    SQL injeciton preventions

--- php/DB/Userinsert.class.php
+++ php/DB/Userinsert.class.php
@@ -10,18 +10,19 @@ class Userinsert{
 	
 function insert_user($sql, $gebruikersnaam, $voornaam, $achternaam, $tussenvoegsel, $date, $email, $straatnaam, $huisnummer, $postcode, $woonplaats, $geslacht, $rechten, $wachtwoord){
 
-	echo $gebruikersnaam."<br>";
-	echo $voornaam."<br>";
-	echo $achternaam."<br>";
-	echo $tussenvoegsel."<br>";
-	echo $email."<br>";
-	echo $straatnaam."<br>";
-	echo $huisnummer."<br>";
-	echo $postcode."<br>";
-	echo $woonplaats."<br>";
-	echo $geslacht."<br>";
-	echo $rechten."<br>";
-	echo $wachtwoord."<br>";
+	$gebruikersnaam = mysqli_real_escape_string($gebruikersnaam);
+	$voornaam = mysqli_real_escape_string($voornaam);
+	$achternaam = mysqli_real_escape_string($achternaam);
+	$tussenvoegsel = mysqli_real_escape_string($tussenvoegsel);
+	$email = mysqli_real_escape_string($email);
+	$straatnaam = mysqli_real_escape_string($straatnaam);
+	$huisnummer = mysqli_real_escape_string($huisnummer);
+	$postcode = mysqli_real_escape_string($postcode);
+	$woonplaats = mysqli_real_escape_string($woonplaats);
+	$geslacht = mysqli_real_escape_string($geslacht);
+	$rechten = mysqli_real_escape_string($rechten);
+	$wachtwoord = mysqli_real_escape_string($wachtwoord);
+	$date = mysqli_real_escape_string($date);
 		
 	$result = mysqli_query($sql, "SELECT 1 FROM account WHERE email = '$email'");
 	if ($result && mysqli_num_rows($result) > 0)
@@ -33,7 +34,7 @@ function insert_user($sql, $gebruikersnaam, $voornaam, $achternaam, $tussenvoegs
 	{
 		echo "NIET geregitreerd";
 		$query = "INSERT INTO account (gebruikersnaam, voornaam, achternaam, tussenvoegsel, geboortedatum, email, straatnaam, huisnummer, postcode, woonplaats, geslacht, rechten, wachtwoord) VALUES ('$gebruikersnaam', '$voornaam', '$achternaam', '$tussenvoegsel' , '$date', '$email' , '$straatnaam', '$huisnummer', '$postcode', '$woonplaats', '$geslacht', '$rechten', '$wachtwoord')";
-		if (mysqli_query($sql, $query)) {
+		if ($sql->doSQL($query)) {
 			header('location: ../View/SuccesPage.php');
 		} else {
 		echo mysqli_error($sql);
