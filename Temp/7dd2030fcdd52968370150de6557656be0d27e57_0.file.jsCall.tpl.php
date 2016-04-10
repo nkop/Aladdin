c1de3b5e0e21dd6706b331c2cce5bcb03405c582
ferryzijl <fjgzijlmans@hotmail.com>
2016-04-10 19:05:05 +0200

    added some new features

--- Temp/7dd2030fcdd52968370150de6557656be0d27e57_0.file.jsCall.tpl.php
+++ Temp/7dd2030fcdd52968370150de6557656be0d27e57_0.file.jsCall.tpl.php
@@ -1,17 +1,17 @@
 <?php
-/* Smarty version 3.1.29, created on 2016-04-08 14:56:56
+/* Smarty version 3.1.29, created on 2016-04-10 19:04:03
   from "C:\xampp\htdocs\Aladdin\php\view\Assets\php\jsCall.tpl" */
 
 if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl, array (
   'has_nocache_code' => false,
   'version' => '3.1.29',
-  'unifunc' => 'content_5707aa98e809d5_27345757',
+  'unifunc' => 'content_570a878381fcd5_43286283',
   'file_dependency' => 
   array (
     '7dd2030fcdd52968370150de6557656be0d27e57' => 
     array (
       0 => 'C:\\xampp\\htdocs\\Aladdin\\php\\view\\Assets\\php\\jsCall.tpl',
-      1 => 1460118100,
+      1 => 1460307713,
       2 => 'file',
     ),
   ),
@@ -19,7 +19,7 @@ if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl,
   array (
   ),
 ),false)) {
-function content_5707aa98e809d5_27345757 ($_smarty_tpl) {
+function content_570a878381fcd5_43286283 ($_smarty_tpl) {
 ?>
 <!-- JS Global Compulsory -->			
 <?php echo '<script'; ?>
@@ -50,10 +50,7 @@ function content_5707aa98e809d5_27345757 ($_smarty_tpl) {
 <?php echo '<script'; ?>
  type="text/javascript" src="../view/assets/plugins/layer-slider/layerslider/js/layerslider.kreaturamedia.jquery.js"><?php echo '</script'; ?>
 >
-<!-- JS Customization -->
-<?php echo '<script'; ?>
- type="text/javascript" src="../view/assets/js/custom.js"><?php echo '</script'; ?>
->
+
 <!-- JS Page Level -->           
 <?php echo '<script'; ?>
  type="text/javascript" src="../view/assets/js/app.js"><?php echo '</script'; ?>
@@ -76,6 +73,11 @@ function content_5707aa98e809d5_27345757 ($_smarty_tpl) {
         OwlRecentWorks.initOwlRecentWorksV2();
     });
 <?php echo '</script'; ?>
+>
+
+<!-- JS Customization -->
+<?php echo '<script'; ?>
+ type="text/javascript" src="../view/assets/js/custom.js"><?php echo '</script'; ?>
 >
 <!--[if lt IE 9]>
     <?php echo '<script'; ?>
