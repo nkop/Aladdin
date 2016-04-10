40a56d135567560a53cec5beb71b591d7e6c2b0e
Mick <mdepeinder@gmail.com>
2016-04-10 11:44:23 +0200

    Committing now

--- php/Model/DB/Userinsert.class.php
+++ php/Model/DB/Userinsert.class.php
@@ -1,5 +1,5 @@
 <?php
-include ("../../Controller/Smarty/header.php");
+
 class Userinsert{
 
 	function calculateAge($datetime){
@@ -10,6 +10,7 @@ class Userinsert{
 	}
 
 function insert_user($sql, $gebruikersnaam, $voornaam, $achternaam, $tussenvoegsel, $date, $email, $straatnaam, $huisnummer, $postcode, $woonplaats, $geslacht, $rechten, $wachtwoord){
+
 
 	$gebruikersnaam = mysqli_real_escape_string($sql, $gebruikersnaam);
 	$voornaam = mysqli_real_escape_string($sql, $voornaam);
@@ -29,16 +30,17 @@ function insert_user($sql, $gebruikersnaam, $voornaam, $achternaam, $tussenvoegs
 	$result = mysqli_query($sql, "SELECT 1 FROM account WHERE email = '$email'");
 	if ($result && mysqli_num_rows($result) > 0)
 	{
-		$smarty->display("../../View/ErrorPage.tpl");
+		header('location: ../../Controller/RegisterController.php/?status=fail');
 	}
 	else
 	{
-		echo "NIET geregitreerd";
+
 		$query = "INSERT INTO account (gebruikersnaam, voornaam, achternaam, tussenvoegsel, geboortedatum, email, straatnaam, huisnummer, postcode, woonplaats, geslacht, rechten, wachtwoord) VALUES ('$gebruikersnaam', '$voornaam', '$achternaam', '$tussenvoegsel' , '$date', '$email' , '$straatnaam', '$huisnummer', '$postcode', '$woonplaats', '$geslacht', '$rechten', '$wachtwoord')";
 		if (mysqli_query($sql, $query)) {
-			$smarty->display("../../View/SuccesPage.tpl");
+			header('location: ../../Controller/RegisterController.php/?status=success');
 		} else {
 		echo mysqli_error($sql);
+		header('location: ../../Controller/RegisterController.php/?status=fail');
 			}
 	}
 
