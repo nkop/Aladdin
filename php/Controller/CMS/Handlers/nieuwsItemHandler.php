2d75baee5b71cd5a01b7764cacbab358157374eb
ferryzijl <fjgzijlmans@hotmail.com>
2016-04-13 17:51:42 +0200

    jo

--- php/Controller/CMS/Handlers/nieuwsItemHandler.php
+++ php/Controller/CMS/Handlers/nieuwsItemHandler.php
@@ -2,18 +2,21 @@
 if (isset($_GET['nieuwsItemID']) && isset($_GET['action'])){
 	$nieuwsitemID = $_GET['nieuwsItemID'];
 	$action = $_GET['action'];
+	
+	switch ($action){
+		case "remove":
+			//remove het nieuwitem
+			header('Location: ../Controllers/shoppingCart.php');
+			break;
+		case "edit":
+			//link naar de edit page
+			header('Location: ../Controllers/shoppingCart.php');
+			break;
+		default:
+			header('Location: ../Controllers/shoppingCart.php');
+			break;
+	}
 }
-switch ($action){
-	case "remove":
-		//remove het nieuwitem
-		header('Location: ../Controllers/shoppingCart.php');
-		break;
-	case "edit":
-		//link naar de edit page
-		header('Location: ../Controllers/shoppingCart.php');
-		break;
-	default:
-		header('Location: ../Controllers/shoppingCart.php');
-		break;
-}
+
+
 ?>
