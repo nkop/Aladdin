2c99e5b70a6b5d572a5cd109bdc719b1252c9eaa
Mick <mdepeinder@gmail.com>
2016-05-18 13:26:44 +0200

    Revert "Pushing cms login"
    This reverts commit 1e4565baa67fada35e16b3dd982f0f21aabab208.

--- php/admin.php
+++ php/admin.php
@@ -13,7 +13,6 @@ require('Controller/CMS/editFaqController.php');
 require('Controller/CMS/editHomeController.php');
 require('Controller/CMS/tagController.php');
 require('Controller/CMS/editRegulationsController.php');
-require('Controller/CMS/cmsLoginController.php');
 
 
 $controller;
@@ -29,10 +28,10 @@ if(isset($_GET["controller"]))
 		$controller = new $controllerName();  //controller = new TodoController();
 		else
 			//TODO: open 404 page
-			$controller = new CmsLoginController();
+			$controller = new DashboardController();
 }
 else{
-	$controller = new CmsLoginController();
+	$controller = new DashboardController();
 }
 if(isset($_GET["action"]))
 {
