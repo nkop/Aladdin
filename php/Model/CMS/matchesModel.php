7650480b94bb7a6b07c76f052423cc4ef14150ba
nkop <nkop1@avans.nl>
2016-06-01 15:41:16 +0200

    cms matchen done

--- php/Model/CMS/matchesModel.php
+++ php/Model/CMS/matchesModel.php
@@ -2,9 +2,7 @@
 /*
  * @author Niels Kop
  */
-
 include_once '/../DB/Database.class.php';
-
 class MatchesModel {
 	// gathers all the matchids, wishes and talents from matches that haven't been approved yet
 	function GatherData() {
@@ -19,19 +17,30 @@ WHERE m.status = 1";
 		return $result;
 	}
 	// accepts a match by setting the status to 2: accepted
-	function AcceptMatch($matchid){
+	function AcceptMatch($matchid) {
 		$db = Database::getInstance ();
 		$sql = $db->getConnection ();
+		
+		$wensid = $sql->query ( "SELECT wensenid FROM `match` WHERE matchid = '$matchid'" );
+		$talentid = $sql->query ( "SELECT talentid FROM `match` WHERE matchid = '$matchid'" );
+		foreach ( $talentid as $item ) {
+			$talentid = $item ["talentid"];
+		}
+		foreach ( $wensid as $item ) {
+			$wensid = $item ["wensenid"];
+		}
+		$updatewens = $sql->query ( "UPDATE `wens` SET `status` = 2 WHERE wensenid = '$wensid'" );
+		$updatetalent = $sql->query ( "UPDATE `talent` SET `status` = 2 WHERE talent = '$talentid'" );
 		
 		$query = "UPDATE `match` SET `status` = 2 WHERE matchid = '$matchid'";
 		return $sql->query($query);
 	}
 	// declines a match by setting the status to 3: declined
-	function DeclineMatch($matchid){
+	function DeclineMatch($matchid) {
 		$db = Database::getInstance ();
 		$sql = $db->getConnection ();
 		
 		$query = "UPDATE `match` SET `status` = 3 WHERE matchid = '$matchid'";
-		return $sql->query($query);
+		return $sql->query ( $query );
 	}
 }
