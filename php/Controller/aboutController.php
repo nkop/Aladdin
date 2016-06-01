2c518d06dcc16e75c56ec2878f7a472d3c85ee5a
ferryzijl <fjgzijlmans@hotmail.com>
2016-06-02 01:01:14 +0200

    fixed about us

--- php/Controller/aboutController.php
+++ php/Controller/aboutController.php
@@ -1,13 +1,14 @@
 <?php
 class aboutController{
-
+	private $model = null;
 	
 	function Index($smarty){
 		include_once 'Model/homepageModel.php';
+		$this->model = new homepageModel();
 		if ($smarty == null) {
 			global $smarty;
 		}
-		$smarty->assign('result', getTexts());
+		$smarty->assign('result', $this->model->getTexts());
 		$smarty->display('about.tpl');
 	}
 
